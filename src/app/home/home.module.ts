bad08cc6220f7f27feda39b0961aa92173c8318f
oscarjairIT <oacevedo@dhemax.cl>
2020-07-24 21:19:51 -0400

    Agrega componentes para primera aproximacion Parking

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AppModule } from '../app.module';
 import { HomeComponent } from './home/home.component';
+import { MaterialModule } from '../material.module';
 
 
 
 @NgModule({
   declarations: [HomeComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    MaterialModule
   ]
 })
 export class HomeModule { }
